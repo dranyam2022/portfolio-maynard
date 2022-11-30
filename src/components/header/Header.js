2e80dbaceae245ae7827268f6020076073457832
dranyam2022 <maynardescalante@gmail.com>
2022-11-30 20:14:39 +0800

    changed fullstack to frontend

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import CTA from './CTA'
-import './Header.css'
-import ME from '../../assets/DJV FEC 282-28.jpg'
-import HeaderSocials from './HeaderSocials'
+import React from "react";
+import CTA from "./CTA";
+import "./Header.css";
+import ME from "../../assets/DJV FEC 282-28.jpg";
+import HeaderSocials from "./HeaderSocials";
 
 function Header() {
-    return (
-        <header>
-            <div className='container header__container'>
-                <h5>Hello I'm</h5>
-                <h1>MAYNARD ESCALANTE</h1>
-                <h5 className='text-light'>Fullstack Developer</h5>
-                <CTA />
-                <HeaderSocials />
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>MAYNARD ESCALANTE</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials />
 
-                <div className='me'>
-                    <img src={ME} alt='me' />
-                </div>
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
 
-                <a href='#contact' className='scroll__down'>SCROLL DOWN</a>
-            </div>
-        </header>
-    )
+        <a href="#contact" className="scroll__down">
+          SCROLL DOWN
+        </a>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
